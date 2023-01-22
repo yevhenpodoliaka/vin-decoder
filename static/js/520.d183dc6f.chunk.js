663f1b385555340551ad2ac1210698e71dcc1497
yevhenpodoliaka <93725802+yevhenpodoliaka@users.noreply.github.com>
2023-01-22 14:55:30 +0000

    Deploying to gh-pages from @ yevhenpodoliaka/vin-decoder@6c3a7d87e8a5570744fc1303988db50d33d9fb81 🚀

---
+++ static/js/520.d183dc6f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvin_decoder=self.webpackChunkvin_decoder||[]).push([[520],{520:function(e,n,r){r.r(n);var o,c=r(168),i=r(87),s=r(444),h=r(184),t=(0,s.ZP)(i.rU)(o||(o=(0,c.Z)(["\n    color:white;\n    &:hover{\n        color:blue;\n    }\n"])));n.default=function(){return(0,h.jsxs)(h.Fragment,{children:[(0,h.jsx)("h1",{children:"404 This Page is NotFound !!!"}),(0,h.jsx)(t,{to:"/",children:"Go Home Page"})]})}}}]);
+//# sourceMappingURL=520.d183dc6f.chunk.js.map
